855c15e052096eaa2594f0f481ea293563b13b54
EmanuelJoseCandido <emanueljosecandido@hotmail.com>
2023-03-30 18:23:14 +0100

    :wrench: Adicionado o script para mudança no ranking.

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UpdateCourseRankRequest;
+use App\Models\Course;
 
 class CourseController extends Controller
 {
-    public function rank()
+    /**
+     * Update the ranking of specific courses
+     *
+     * @param  UpdateCourseRankRequest $updateCourseRankRequest
+     * @return void
+     */
+    public function rank(UpdateCourseRankRequest $updateCourseRankRequest): void
     {
-        dd('X');
+        $input = $updateCourseRankRequest->validated();
+        $course = new Course();
+
+        $firstChange = $course->find($input['firstId']);
+        $secondChange = $course->find($input['secondId']);
+
+        $aux = $firstChange->ranking;
+        $firstChange->ranking = $secondChange->ranking;
+        $secondChange->ranking = $aux;
+
+        $firstChange->save();
+        $secondChange->save();
     }
 }
